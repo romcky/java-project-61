8a1721ba9bff2fd327822f21475fb0feb5690aaf
Anton Romanovsky <romcky@gmail.com>
2024-01-12 21:11:30 +0300

    remove in.close()

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Cli {
     public static void nameRequest() {
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         var in = new Scanner(System.in);
         String name = in.next();
         System.out.println("Hello, " + name + "!");
-        in.close();
     }
 }
